e0a97fac22c13111702ba8277e42159a86852229
Bertrand Bichat <bertrand@leto.legal>
2024-11-18 18:23:27 +0100

    setup tests for FetchDeskqDeviceJob

--- spec/jobs/fetch_deskq_device_job_spec.rb
+++ spec/jobs/fetch_deskq_device_job_spec.rb
@@ -1,5 +1,59 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-RSpec.describe FetchDeskqDeviceJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+require "rails_helper"
+
+RSpec.describe(FetchDeskqDeviceJob, type: :job) do
+  let(:devices_service) { instance_double(Deskq::Devices::DevicesService) }
+  let(:device_data) do
+    [
+      { "id" => "1", "color" => "RED", "desk_sync_id" => "sync1" },
+      { "id" => "2", "color" => "GREEN", "desk_sync_id" => "sync2" },
+    ]
+  end
+
+  let!(:desk) { create(:desk, sync_id: "sync1") }
+
+  before do
+    allow(Deskq::Devices::DevicesService).to(receive(:new).and_return(devices_service))
+    allow(devices_service).to(receive(:list).and_return(device_data))
+  end
+
+  it "calls the DevicesService to fetch devices" do
+    described_class.perform_now(desk.id)
+
+    expect(devices_service).to(have_received(:list))
+  end
+
+  it "creates a Deskq::Device for a matching desk" do
+    described_class.perform_now(desk.id)
+
+    device = Deskq::Device.find_by(api_id: "1")
+    expect(device).to(be_present)
+    expect(device.desk_id).to(eq(desk.id))
+    expect(device.color).to(eq("RED"))
+  end
+
+  it "does not create a device if the desk has a device already" do
+    create(:deskq_device, desk: desk)
+
+    described_class.perform_now(desk.id)
+
+    expect(Deskq::Device.count).to(eq(1))
+  end
+
+  it "does not create a device if the desk_sync_id is blank" do
+    desk.update(sync_id: nil)
+
+    described_class.perform_now(desk.id)
+
+    expect(Deskq::Device.count).to(eq(0))
+  end
+
+  it "breaks after creating the first matching device" do
+    described_class.perform_now(desk.id)
+
+    device = Deskq::Device.find_by(api_id: "1")
+    expect(device).to(be_present)
+    expect(Deskq::Device.count).to(eq(1))
+  end
 end
