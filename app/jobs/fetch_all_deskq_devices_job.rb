5688a05ca945a83dfd5cb51e21519c63c05a680e
Bertrand Bichat <bertrand@leto.legal>
2024-11-18 19:07:42 +0100

    update FetchAllDeskqDevicesJob

--- app/jobs/fetch_all_deskq_devices_job.rb
+++ app/jobs/fetch_all_deskq_devices_job.rb
@@ -4,6 +4,13 @@ class FetchAllDeskqDevicesJob < ApplicationJob
   queue_as :default
 
   def perform
+    sync_deskq_devices
+    sync_color_from_desk_bookings
+  end
+
+  private
+
+  def sync_deskq_devices
     devices = Deskq::Devices::DevicesService.new.list
     devices.each do |device|
       desk = Desk.find_by(sync_id: device["desk_sync_id"])
@@ -22,5 +29,10 @@ class FetchAllDeskqDevicesJob < ApplicationJob
 
       new_device.save!
     end
+  end
+
+  def sync_color_from_desk_bookings
+    DeskBooking.where(state: "checked_in").find_each(&:set_device_as_occupied!)
+    DeskBooking.where.not(state: "checked_in").find_each(&:set_device_as_available!)
   end
 end
